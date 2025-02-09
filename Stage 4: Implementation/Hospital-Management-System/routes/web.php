1b97dc4e1dd014fa48c065f08fc568b12661230d
MuchiriKinyua <muchirikinyua2000@gmail.com>
2025-02-09 14:09:42 +0300

    user and admin login

--- Stage 4: Implementation/Hospital-Management-System/routes/web.php
+++ Stage 4: Implementation/Hospital-Management-System/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', [HomeController::class, 'redirect']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
