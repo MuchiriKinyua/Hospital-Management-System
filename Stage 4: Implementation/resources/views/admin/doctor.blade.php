dd88289f057c31968d59addb66501d30eb295672
MuchiriKinyua <muchirikinyua2000@gmail.com>
2025-02-15 13:10:34 +0300

    updating data

--- Stage 4: Implementation/resources/views/admin/doctor.blade.php
+++ Stage 4: Implementation/resources/views/admin/doctor.blade.php
@@ -33,7 +33,7 @@
                 <td>{{$doctor->speciality}}</td>
                 <td>{{$doctor->room}}</td>
                 <td><img height="100" width="100" src="doctorimage/{{$doctor->image}}"></td>
-                <td><a class="btn btn-success" href="">Update</a></td>
+                <td><a class="btn btn-success" href="{{url('updatedoctor', $doctor->id)}}">Update</a></td>
                 <td><a onclick="return confirm('are you sure?')" class="btn btn-danger" href="{{url('deletedoctor', $doctor->id)}}">Delete</a></td>
             </tr>
                 @endforeach
